03437be671aaed3701b0157c1695f66f235bd147
Gihyeon KIM <ken29511@gmail.com>
2021-12-23 19:46:41 +0900

    add function drawing line

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+let x;
+let y;
+
+function startPainting() {
+  painting = true;
+}
 
 function stopPainting() {
   painting = false;
 }
 
 function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
+  x = event.offsetX;
+  y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-function onMouseDown(event) {
-  painting = true;
-}
+function onMouseEnter(event) {
+  x = event.offsetX;
+  y = event.offsetY;
 
-function onMouseUp(event) {
-  stopPainting();
+  ctx.moveTo(x, y);
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseenter", onMouseEnter);
 }
